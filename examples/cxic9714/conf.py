25a1496e9149ab275d51dd16818b81f52ea93686
Filipe Maia <filipe.maia@icm.uu.se>
2015-02-17 21:55:47 +0100

    Add another example

---
+++ examples/cxic9714/conf.py
@@ -0,0 +1,20 @@
+import analysis.event
+import analysis.beamline
+import analysis.pixel_detector
+
+state = {
+    'Facility': 'LCLS',
+    'LCLS/DataSource': '/data/rawdata/LCLS/cxi/cxic9714/xtc/e419-r0203-s00-c00.xtc'
+}
+
+def onEvent(evt):
+#    analysis.event.printKeys(evt)
+#    analysis.event.printNativeKeys(evt)
+    analysis.beamline.printPulseEnergy(evt['pulseEnergies'])
+    analysis.beamline.printPhotonEnergy(evt['photonEnergies'])
+    print "EPICS photon energy = %g eV" %(evt['parameters']['SIOC:SYS0:ML00:AO541'].data)
+#    analysis.pixel_detector.printStatistics(evt['photonPixelDetectors'])
+#    analysis.pixel_detector.printStatistics(evt['ionTOFs'])
+#    analysis.event.printID(evt['eventID'])
+    analysis.event.printProcessingRate(evt)
+
