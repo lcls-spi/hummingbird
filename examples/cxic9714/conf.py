6b12f21e1724b9854418ca2a5438718228bc207e
Filipe Maia <filipe.maia@icm.uu.se>
2015-02-23 12:08:45 +0100

    Initial interface draft

--- examples/cxic9714/conf.py
+++ examples/cxic9714/conf.py
@@ -1,18 +1,21 @@
+import time
 import analysis.event
 import analysis.beamline
 import analysis.pixel_detector
 
 state = {
     'Facility': 'LCLS',
-    'LCLS/DataSource': '/data/rawdata/LCLS/cxi/cxic9714/xtc/e419-r0203-s00-c00.xtc'
+    'LCLS/DataSource': '/data/rawdata/LCLS/cxi/cxic9714/xtc/e419-r0263-s03-c00.xtc'
 }
 
 def onEvent(evt):
-    analysis.beamline.printPulseEnergy(evt['pulseEnergies'])
-    analysis.beamline.printPhotonEnergy(evt['photonEnergies'])
-    print "EPICS photon energy = %g eV" %(evt['parameters']['SIOC:SYS0:ML00:AO541'].data)
-    analysis.pixel_detector.printStatistics(evt['photonPixelDetectors'])
-    analysis.pixel_detector.printStatistics(evt['ionTOFs'])
-    analysis.event.printID(evt['eventID'])
+    analysis.beamline.plotPulseEnergy(evt['pulseEnergies'])
+#    analysis.beamline.printPulseEnergy(evt['pulseEnergies'])
+#    analysis.beamline.printPhotonEnergy(evt['photonEnergies'])
+#    print "EPICS photon energy = %g eV" %(evt['parameters']['SIOC:SYS0:ML00:AO541'].data)
+#    analysis.pixel_detector.printStatistics(evt['photonPixelDetectors'])
+#    analysis.pixel_detector.printStatistics(evt['ionTOFs'])
+#    analysis.event.printID(evt['eventID'])
+    analysis.event.plotFiducial(evt['eventID'])
     analysis.event.printProcessingRate(evt)
-
+    time.sleep(0.01)
