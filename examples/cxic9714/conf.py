8a212c21d57ec6385d5a381f9b6518248ad7170e
Filipe Maia <filipe.c.maia@gmail.com>
2015-03-04 19:15:39 -0800

    Access psana created values by key

--- examples/cxic9714/conf.py
+++ examples/cxic9714/conf.py
@@ -13,11 +13,12 @@ state = {
 
 def onEvent(evt):
     analysis.event.printNativeKeys(evt)
-#    analysis.beamline.plotPulseEnergy(evt['pulseEnergies'])
-#    analysis.beamline.printPulseEnergy(evt['pulseEnergies'])
-#    analysis.beamline.printPhotonEnergy(evt['photonEnergies'])
-#    print "EPICS photon energy = %g eV" %(evt['parameters']['SIOC:SYS0:ML00:AO541'].data)
-    analysis.pixel_detector.printStatistics(evt['photonPixelDetectors'])
+    #    analysis.beamline.plotPulseEnergy(evt['pulseEnergies'])
+    #    analysis.beamline.printPulseEnergy(evt['pulseEnergies'])
+    #    analysis.beamline.printPhotonEnergy(evt['photonEnergies'])
+    #    print "EPICS photon energy = %g eV" %(evt['parameters']['SIOC:SYS0:ML00:AO541'].data)
+    #    analysis.pixel_detector.printStatistics(evt['photonPixelDetectors'])
+    analysis.pixel_detector.printStatistics(evt['reconstructed'])
     analysis.pixel_detector.plotImages(evt['photonPixelDetectors'])
 #    analysis.pixel_detector.printStatistics(evt['ionTOFs'])
 #    analysis.event.printID(evt['eventID'])
