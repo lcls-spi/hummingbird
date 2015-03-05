24f74f704ed9dabd79e16e7020b2e0149c4d01d5
daurer <benedikt.daurer@icm.uu.se>
2015-03-05 11:41:01 -0800

    now it is possible to run many instances of mean photon maps

--- examples/cxi86415/conf.py
+++ examples/cxi86415/conf.py
@@ -1,3 +1,4 @@
+
 import time
 import analysis.event
 import analysis.beamline
@@ -7,36 +8,72 @@ import analysis.pixel_detector
 state = {
     'Facility': 'LCLS',
 
-    #'LCLS/DataSource': '/reg/neh/home2/benedikt/data/cxi/cxic9714/xtc/e419-r0199-s09-c00.xtc'
-    #'LCLS/DataSource': 'shmem=CXI.0:stop=no',
-    'LCLS/DataSource': 'exp=cxi86415:run=2:xtc',
+    'LCLS/DataSource': 'shmem=CXI.0:stop=no',
+    #'LCLS/DataSource': 'exp=cxi86415:run=2:xtc',
     'LCLS/PsanaConf': 'small_back.cfg',
 
-    'aduThreshold': 0,
-    'aduPhoton': 1,
+    'aduThreshold': 15,
+    'aduPhoton': 20,
 
-    'meanPhotonMap/initialize': True,
-    'meanPhotonMap/paramXmin': 0,
-    'meanPhotonMap/paramXmax': 1000,
-    'meanPhotonMap/paramYmin': -20000,
-    'meanPhotonMap/paramYmax': -15000,
-    'meanPhotonMap/paramXbin': 10,
-    'meanPhotonMap/paramYbin': 10,
-    'meanPhotonMap/updateRate': 100
-}
+    'meanPhotonMap': {
+        'aperture1': {
+            'paramXmin': 0,
+            'paramXmax': 1000,
+            'paramYmin': -20000,
+            'paramYmax': -15000,
+            'paramXbin': 10,
+            'paramYbin': 10,
+            'updateRate': 100
+            },
+        'aperture2': {
+            'paramXmin': 0,
+            'paramXmax': 1000,
+            'paramYmin': -20000,
+            'paramYmax': -15000,
+            'paramXbin': 10,
+            'paramYbin': 10,
+            'updateRate': 100
+            },
+        'aperture3': {
+            'paramXmin': 0,
+            'paramXmax': 1000,
+            'paramYmin': -20000,
+            'paramYmax': -15000,
+            'paramXbin': 10,
+            'paramYbin': 10,
+            'updateRate': 100
+            }
+        }
+    }
 
 def onEvent(evt):
-    #print evt.nativeKeys()
-    #print evt.keys()
-    #print evt['parameters'].keys()
-    #print evt['calibrated'].keys(
-    print evt['parameters']['ap1_x'].data
-    print evt['parameters']['ap1_y'].data
+    #print "Native keys: ", evt.nativeKeys()
+    #print "Hummingbird keys: ", evt.keys()
 
+    # Detector calibration and other stuff
     analysis.pixel_detector.plotDetector(evt['calibrated']['CsPad Dg3 [calibrated]'])
-    nrPhotons = analysis.pixel_detector.countNrPhotons(evt['calibrated']['CsPad Dg3 [calibrated]'].data)
-    analysis.background.plotMeanPhotonMap(nrPhotons, evt['parameters']['ap1_x'].data, evt['parameters']['ap1_y'].data)
+
+    # Counting photons on the small back detector
+    nrPhotons = analysis.pixel_detector.countNrPhotons(evt['calibrated']['CsPad Dg3 [calibrated]'])
+
+    # Mean Photon Map for Alignment of Aperture 1
+    print "Aperture 1, position in x: ", evt['parameters']['ap1_x'].data
+    print "Aperture 1, position in y: ", evt['parameters']['ap1_y'].data
+    analysis.background.plotMeanPhotonMap('aperture1', state["meanPhotonMap"]["aperture1"], nrPhotons, evt['parameters']['ap1_x'], evt['parameters']['ap1_y'])
+
+    # Mean Photon Map for Alignment of Aperture 3
+    print "Aperture 2, position in x: ", evt['parameters']['ap2_x'].data
+    print "Aperture 2, position in y: ", evt['parameters']['ap2_y'].data
+    analysis.background.plotMeanPhotonMap('aperture2', state["meanPhotonMap"]["aperture2"], nrPhotons, evt['parameters']['ap2_x'], evt['parameters']['ap2_y'])
     
+    # Mean Photon Map for Alignment of Aperture 3
+    print "Aperture 3, position in x: ", evt['parameters']['ap3_x'].data
+    print "Aperture 3, position in y: ", evt['parameters']['ap3_y'].data
+    analysis.background.plotMeanPhotonMap('aperture3', state["meanPhotonMap"]["aperture3"], nrPhotons, evt['parameters']['ap3_x'], evt['parameters']['ap3_y'])
+    
+    # How fast are we processing the data?
+    analysis.event.printProcessingRate(evt)
+
     #print evt.keys()
     #print evt['photonPixelDetectors'].keys()
     #analysis.beamline.plotPulseEnergy(evt['pulseEnergies'])
@@ -47,6 +84,6 @@ def onEvent(evt):
 #    analysis.pixel_detector.printStatistics(evt['ionTOFs'])
 #    analysis.event.printID(evt['eventID'])
     #analysis.event.plotFiducial(evt['eventID'])
-    analysis.event.printProcessingRate(evt)
+
     #analysis.pixel_detector.plotImages(evt['reconstructed'])
     #time.sleep(1)
