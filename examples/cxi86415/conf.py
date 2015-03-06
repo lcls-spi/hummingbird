5c9ea7fad79a053f103ec95c2cdb336857bfabb5
daurer <benedikt.daurer@icm.uu.se>
2015-03-05 20:56:12 -0800

    running analysis from Ds2 instead of Dg3

--- examples/cxi86415/conf.py
+++ examples/cxi86415/conf.py
@@ -11,8 +11,9 @@ state = {
 
     'LCLS/DataSource': 'shmem=CXI.0:stop=no',
     #'LCLS/DataSource': 'exp=cxi86415:run=2:xtc',
-    'LCLS/PsanaConf': 'small_back.cfg',
-
+    #'LCLS/PsanaConf': 'small_back.cfg',
+    'LCLS/PsanaConf': 'back.cfg',
+    
     'aduThreshold': 20,
     'aduPhoton': 1,
     'hitscoreMinCount':200,
@@ -27,7 +28,7 @@ state = {
             'paramYmax':  10000,
             'paramXbin':  100,
             'paramYbin':  100,
-            'updateRate': 100
+            'updateRate': 500
             },
         'aperture2': {
             'paramXmin': -10000,
@@ -36,7 +37,7 @@ state = {
             'paramYmax':  10000,
             'paramXbin':  100,
             'paramYbin':  100,
-            'updateRate': 100
+            'updateRate': 500
             },
         'aperture3': {
             'paramXmin': -10000,
@@ -45,7 +46,7 @@ state = {
             'paramYmax':  10000,
             'paramXbin':  100,
             'paramYbin':  100,
-            'updateRate': 100
+            'updateRate': 500
             }
         }
     }
@@ -54,6 +55,7 @@ def onEvent(evt):
     #print "Native keys: ", evt.nativeKeys()
     #print "Hummingbird keys: ", evt.keys()
     #print "EPICS keys: ", evt["parameters"].keys()
+    #print "Detectors: ", evt["photonPixelDetectors"].keys()
 
     # Detector calibration and other stuff
     #analysis.pixel_detector.plotDetector(evt['calibrated']['CsPad Dg3 [calibrated]'])
@@ -63,9 +65,16 @@ def onEvent(evt):
     analysis.beamline.plotPulseEnergy(evt["pulseEnergies"])
     pulseEnergy = analysis.beamline.averagePulseEnergies(evt["pulseEnergies"])
 
+    # Reshape CSPAD Ds2 detector
+    #print evt["calibrated"]["CsPad Ds2 [calibrated]"].data
+    evt["photonPixelDetectors"]["CsPad Ds2 central"] =  analysis.pixel_detector.reshape_detector(evt["calibrated"]["CsPad Ds2 [calibrated]"])
+
+    #print evt["photonPixelDetectors"]["CsPad Ds2 central"]
     # Counting photons on the small back detector
-    nrPhotons = analysis.pixel_detector.countNrPhotons(evt['calibrated']['CsPad Dg3 [calibrated]'])
-    analysis.pixel_detector.plotNrPhotons("CsPad2x2 - Nr. of Photons (adup = %d, th = %d)" %(state["aduPhoton"], state["aduThreshold"]), nrPhotons)
+    #nrPhotons = analysis.pixel_detector.countNrPhotons(evt['calibrated']['CsPad Dg3 [calibrated]'].data)
+    #analysis.pixel_detector.plotNrPhotons("CsPad2x2 - Nr. of Photons (adup = %d, th = %d)" %(state["aduPhoton"], state["aduThreshold"]), nrPhotons)
+    nrPhotons = analysis.pixel_detector.countNrPhotons(evt['photonPixelDetectors']['CsPad Ds2 central'])
+    analysis.pixel_detector.plotNrPhotons("CsPad Ds2 - Nr. of Photons (adup = %d, th = %d)" %(state["aduPhoton"], state["aduThreshold"]), nrPhotons)
 
     # Mean Photon Map for Alignment of Aperture 1
     #print "Aperture 1, position in x: ", evt['parameters']['ap1_x'].data
@@ -89,7 +98,8 @@ def onEvent(evt):
     analysis.background.plotAperturePos(evt['parameters']['ap3_y'])
 
     # Hitfinding
-    hit, hitscore = analysis.hitfinding.countLitPixels(evt['calibrated']['CsPad Dg3 [calibrated]'])
+    # hit, hitscore = analysis.hitfinding.countLitPixels(evt['calibrated']['CsPad Dg3 [calibrated]'])
+    hit, hitscore = analysis.hitfinding.countLitPixels(evt['photonPixelDetectors']['CsPad Ds2 central'])
     analysis.hitfinding.plotHitscore(hitscore)
 
     # How fast are we processing the data?
