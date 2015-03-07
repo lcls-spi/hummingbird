23b45404f83a4bd8b64ba8be489f43bf9ff6dd1e
daurer <benedikt.daurer@icm.uu.se>
2015-03-06 17:55:38 -0800

    small changes

--- examples/cxi86415/conf.py
+++ examples/cxi86415/conf.py
@@ -12,8 +12,8 @@ state = {
     'LCLS/DataSource': 'shmem=CXI.0:stop=no',
     #'LCLS/DataSource': 'exp=cxi86415:run=2:xtc',
     #'LCLS/PsanaConf': 'small_back.cfg',
-    #'LCLS/PsanaConf': 'back.cfg',
-    'LCLS/PsanaConf': 'back_with_geometry.cfg',
+    'LCLS/PsanaConf': 'back.cfg',
+    #'LCLS/PsanaConf': 'back_with_geometry.cfg',
 
     'aduThreshold': 20,
     'aduPhoton': 1,
@@ -23,31 +23,37 @@ state = {
 
     'meanPhotonMap': {
         'aperture1': {
-            'paramXmin': -10000,
-            'paramXmax':  10000,
-            'paramYmin': -10000,
-            'paramYmax':  10000,
-            'paramXstep': 100,
-            'paramYstep': 100,
-            'updateRate': 500
+            'paramXmin':  8000-1000,
+            'paramXmax':  8000+1000,
+            'xlabel': 'position in x [mm]',
+            'paramYmin':  -7700-250,
+            'paramYmax':  -7700+250,
+            'ylabel': 'position in y [mm]',
+            'paramXstep': 25,
+            'paramYstep': 25,
+            'updateRate': 100
             },
         'aperture2': {
-            'paramXmin': -10000,
-            'paramXmax':  10000,
-            'paramYmin': -10000,
-            'paramYmax':  10000,
-            'paramXstep': 100,
-            'paramYstep': 100,
-            'updateRate': 500
+            'paramXmin':  9700-250,
+            'paramXmax':  9700+250,
+            'xlabel': 'position in x [mm]',
+            'paramYmin':  1200-250,
+            'paramYmax':  1200+250,
+            'ylabel': 'position in y [mm]',
+            'paramXstep': 25,
+            'paramYstep': 25,
+            'updateRate': 100
             },
         'aperture3': {
-            'paramXmin': -10000,
-            'paramXmax':  10000,
-            'paramYmin': -10000,
-            'paramYmax':  10000,
-            'paramXstep': 100,
-            'paramYstep': 100,
-            'updateRate': 500
+            'paramXmin':   9900-250,
+            'paramXmax':   9900+250,
+            'xlabel': 'position in x [mm]',
+            'paramYmin':   -300-250,
+            'paramYmax':   -300+250,
+            'ylabel': 'position in y [mm]',
+            'paramXstep': 25,
+            'paramYstep': 25,
+            'updateRate': 100
             }
         }
     }
@@ -76,40 +82,40 @@ def onEvent(evt):
 
     # Reshape CSPAD Ds2 detector
     #print evt["calibrated"]["CsPad Ds2 [calibrated]"].data
-    #cspad_central =  analysis.pixel_detector.reshape_detector(cspad)
+    cspad_central =  analysis.pixel_detector.reshape_detector(cspad)
 
     #print evt["photonPixelDetectors"]["CsPad Ds2 central"]
     # Counting photons on the small back detector
     #nrPhotons = analysis.pixel_detector.countNrPhotons(evt['calibrated']['CsPad Dg3 [calibrated]'].data)
     #analysis.pixel_detector.plotNrPhotons("CsPad2x2 - Nr. of Photons (adup = %d, th = %d)" %(state["aduPhoton"], state["aduThreshold"]), nrPhotons)
-    #nrPhotons = analysis.pixel_detector.countNrPhotons(cspad_central)
-    #analysis.pixel_detector.plotNrPhotons("CsPad Ds2 - Nr. of Photons (adup = %d, th = %d)" %(state["aduPhoton"], state["aduThreshold"]), nrPhotons)
+    nrPhotons = analysis.pixel_detector.countNrPhotons(cspad_central)
+    analysis.pixel_detector.plotNrPhotons("CsPad Ds2 - Nr. of Photons (adup = %d, th = %d)" %(state["aduPhoton"], state["aduThreshold"]), nrPhotons)
 
     # Mean Photon Map for Alignment of Aperture 1
     #print "Aperture 1, position in x: ", evt['parameters']['ap1_x'].data
     #print "Aperture 1, position in y: ", evt['parameters']['ap1_y'].data
-    #analysis.background.plotMeanPhotonMap('aperture1', state["meanPhotonMap"]["aperture1"], nrPhotons, evt['parameters']['ap1_x'], evt['parameters']['ap1_y'], pulseEnergy)
+    analysis.background.plotMeanPhotonMap('aperture1', state["meanPhotonMap"]["aperture1"], nrPhotons, evt['parameters']['ap1_x'], evt['parameters']['ap1_y'], pulseEnergy)
     analysis.background.plotAperturePos(evt['parameters']['ap1_x'])
     analysis.background.plotAperturePos(evt['parameters']['ap1_y'])
 
     # Mean Photon Map for Alignment of Aperture 3
     #print "Aperture 2, position in x: ", evt['parameters']['ap2_x'].data
     #print "Aperture 2, position in y: ", evt['parameters']['ap2_y'].data
-    #analysis.background.plotMeanPhotonMap('aperture2', state["meanPhotonMap"]["aperture2"], nrPhotons, evt['parameters']['ap2_x'], evt['parameters']['ap2_y'], pulseEnergy)
+    analysis.background.plotMeanPhotonMap('aperture2', state["meanPhotonMap"]["aperture2"], nrPhotons, evt['parameters']['ap2_x'], evt['parameters']['ap2_y'], pulseEnergy)
     analysis.background.plotAperturePos(evt['parameters']['ap2_x'])
     analysis.background.plotAperturePos(evt['parameters']['ap2_y'])
     
     # Mean Photon Map for Alignment of Aperture 3
     #print "Aperture 3, position in x: ", evt['parameters']['ap3_x'].data
     #print "Aperture 3, position in y: ", evt['parameters']['ap3_y'].data
-    #analysis.background.plotMeanPhotonMap('aperture3', state["meanPhotonMap"]["aperture3"], nrPhotons, evt['parameters']['ap3_x'], evt['parameters']['ap3_y'], pulseEnergy)
+    analysis.background.plotMeanPhotonMap('aperture3', state["meanPhotonMap"]["aperture3"], nrPhotons, evt['parameters']['ap3_x'], evt['parameters']['ap3_y'], pulseEnergy)
     analysis.background.plotAperturePos(evt['parameters']['ap3_x'])
     analysis.background.plotAperturePos(evt['parameters']['ap3_y'])
 
     # Hitfinding
     # hit, hitscore = analysis.hitfinding.countLitPixels(evt['calibrated']['CsPad Dg3 [calibrated]'])
-    #hit, hitscore = analysis.hitfinding.countLitPixels(cspad)
-    #analysis.hitfinding.plotHitscore(hitscore)
+    hit, hitscore = analysis.hitfinding.countLitPixels(cspad_central)
+    analysis.hitfinding.plotHitscore(hitscore)
 
     # How fast are we processing the data?
     analysis.event.printProcessingRate(evt)
