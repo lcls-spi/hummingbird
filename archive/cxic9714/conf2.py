4a25620cbe97e6b9a50946527b18228aa2cc16a9
Max Hantke <maxhantke@gmail.com>
2015-06-04 18:41:34 +0200

    archieve: Background buffer implemented and proper plotting of radial averages.

--- archive/cxic9714/conf2.py
+++ archive/cxic9714/conf2.py
@@ -1,5 +1,6 @@
 import os
 import time
+import numpy,collections
 import utils.reader
 import utils.array
 import simulation.cxi
@@ -10,6 +11,7 @@ import analysis.sizing
 import plotting.line
 import plotting.image
 import plotting.correlation
+import ipc
 
 key_to_data = "entry_1/image_1/data"
 key_to_pulse_energy = "LCLS/f_11_ENRC"
@@ -64,7 +66,6 @@ histogramCCD = {
 heatmapInjector = {
     'xmin': 0,
     'xmax': 10,
-
     'xbins': 20,
     'ymin': 20,
     'ymax': 120,
@@ -103,14 +104,21 @@ sizingParams = {
     'd0':100,
     'i0':1,
     'mask_radius':300/binning,
-    'downsampling':1,
     'brute_evals':40,
     'photon_counting':True}
 
+# Geometry and mask
 this_dir = os.path.dirname(os.path.realpath(__file__))
 greader = utils.reader.GeometryReader(this_dir + "/geometry.h5")
 mreader = utils.reader.MaskReader(this_dir + "/mask.h5","/data/data")
 mask    = utils.array.assembleImage(greader.x, greader.y, mreader.boolean_mask, nx=414, ny=414, dtype='bool')
+
+# Background buffer
+iD = 0
+nD = 100
+D = collections.deque(maxlen=nD)
+bg = None
+
 
 def onEvent(evt):
 
@@ -143,7 +151,17 @@ def onEvent(evt):
     plotting.line.plotHistory(evt["parameters"]["injector_posz"], label='Position in Z [um]')
     
     # Perform sizing on hits
-    if evt["analysis"]["isHit - CCD"]:
+    if not evt["analysis"]["isHit - CCD"]:
+        D.append(evt["photonPixelDetectors"]["CCD"].data)
+        global iD
+        global nD
+        global bg
+        iD += 1
+        if (iD % nD) == 0:
+            print "Calculating new background average."
+            bg = numpy.array(D).mean(axis=0)
+            ipc.new_data("background average", bg)
+    else:
 
         print "It's a hit"
 
@@ -179,13 +197,23 @@ def onEvent(evt):
             # Fitting sphere model to get size and intensity
             analysis.sizing.fitSphereRadial(evt, "analysis", "radial distance - CCD", "radial average - CCD", **dict(modelParams, **sizingParams))
             t_size = time.time()-t0
-            plotting.line.plotTrace(evt["analysis"]["radial average - CCD"], evt["analysis"]["radial distance - CCD"])
-
+            
         # Fitting model
         analysis.sizing.sphereModel(evt, "analysis", "offCenterX", "offCenterY", "diameter", "intensity", m.shape, poisson=True, **modelParams)
 
+        if mode == "radial":
+            analysis.pixel_detector.radial(evt, "analysis", "fit", mask=m, cx=cx, cy=cy)          
+            # 1D arrays have to have same length, otherwise histoty keeping gets messed up
+            rlen = 100
+            ipc.new_data("radial fit", numpy.array([evt["analysis"]["radial distance - fit"].data.ravel()[:rlen], evt["analysis"]["radial average - fit"].data.ravel()[:rlen]], copy=False))
+            ipc.new_data("radial CCD", numpy.array([evt["analysis"]["radial distance - CCD"].data.ravel()[:rlen], evt["analysis"]["radial average - CCD"].data.ravel()[:rlen]], copy=False))
+            
         t_all = t_center + t_size
         print "Time: %e sec (center / size : %.2f%% / %.2f%%)" % (t_all, 100.*t_center/t_all, 100.*t_size/t_all)
+
+        # Error parameter
+        diff = (evt[ccd_type][ccd_key].data-evt["analysis"]["fit"].data)[m].sum()/float(evt[ccd_type][ccd_key].data[m].sum())
+        ipc.new_data("fit diff", diff)
         
         plotting.line.plotHistory(evt["analysis"]["offCenterX"])
         plotting.line.plotHistory(evt["analysis"]["offCenterY"])
@@ -197,7 +225,7 @@ def onEvent(evt):
         s0 = evt["analysis"]["diameter"].data
         I0 = evt["analysis"]["intensity"].data
         msg_glo = "diameter = %.2f nm, \nintensity = %.2f mJ/um2" % (s0, I0)
-        msg_fit = "Fit result: \ndiameter = %.2f nm, \nintensity = %.2f mJ/um2" % (s0, I0)
+        msg_fit = "Fit result: \ndiameter = %.2f nm, \nintensity = %.2f mJ/um2, diff=%f" % (s0, I0, diff)
 
         # Plot the glorious shots
         plotting.image.plotImage(evt[ccd_type][ccd_key], msg=msg_glo, log=True, mask=m)
@@ -210,3 +238,6 @@ def onEvent(evt):
 
         # Plot heatmap of center positions
         plotting.correlation.plotHeatmap(evt["analysis"]["offCenterX"], evt["analysis"]["offCenterY"], **heatmapCenterPos)
+
+
+        
