75439b09fb230a62f2ac8e3ee6701be82f10b410
daurer <benedikt.daurer@icm.uu.se>
2015-06-07 15:29:46 -0700

    Making archive/cxi86415/conf.py work again

--- archive/cxi86415/conf.py
+++ archive/cxi86415/conf.py
@@ -6,7 +6,9 @@ import analysis.beamline
 import analysis.background
 import analysis.hitfinding
 import analysis.pixel_detector
-import plotting.maps
+import plotting.image
+import plotting.line
+import plotting.correlation
 
 # Configuration of state
 # ----------------------
@@ -15,7 +17,7 @@ state = {
 
     #'LCLS/DataSource': 'shmem=4_3_psana_CXI.0:stop=no',
     #'LCLS/DataSource': 'shmem=CXI.0:stop=no',
-    'LCLS/DataSource': 'exp=cxi86415:run=17:xtc',
+    'LCLS/DataSource': 'exp=cxi86415:run=19:xtc',
 
     #'LCLS/PsanaConf': 'Dg3.cfg',
     #'LCLS/PsanaConf': 'Dg3_recons.cfg',
@@ -31,7 +33,7 @@ state = {
 # ----------
 aduThreshold = 20
 aduPhoton    = 1
-hitscoreMinCount = 200
+litPixelThreshold = 200
 detectorUpdateRate = 100
 
 # Configuration of plots
@@ -42,12 +44,12 @@ histogram = {
     'bins': 100}
 
 meanIntensityAperture1 = {
-    'xmin':  8000-2000,
-    'xmax':  8000+2000,
-    'ymin':  -1500-2000,
-    'ymax':  -1500+2000,
+    'xmin':  2000,
+    'xmax':  8000,
+    'ymin':  -10000,
+    'ymax':  -2000,
     'step':  10,
-    'localRadius': 100,
+    'localRadius': 10,
     'overviewStep':100,
     'xlabel': 'position in x [mm]',
     'ylabel': 'position in y [mm]'}
@@ -86,14 +88,15 @@ meanIntensitySample = {
     'ylabel': 'position in y [mm]'}
     
 def onEvent(evt):
-    #print "Native keys: ",      evt.nativeKeys()
+    #print "Native keys: ",      evt.native_keys()
     #print "Hummingbird keys: ", evt.keys()
     #print "EPICS keys: ",       evt["parameters"].keys()
     #print "Detectors: ",        evt["photonPixelDetectors"].keys()
+    #print "Detectors: ",        evt["calibrated"].keys()
 
     # What detector to use
     try:
-        cspad = evt["calibrated"]["CsPad Ds2 [calibrated]"]
+        cspad = evt["calibrated"]["CsPad Ds2[calibrated]"]
         #cspad = evt["calibrated"]["CsPad Dg3 [calibrated]"]
         #cspad = evt["reconstructed"]["CsPad Ds2 [reconstructed]"]
     except TypeError:
@@ -107,68 +110,53 @@ def onEvent(evt):
     # Average Pulse Energy
     # --------------------
     #print evt["pulseEnergies"]
-    evt["pulseEnergies"]["average"] = analysis.beamline.averagePulseEnergy(evt["pulseEnergies"])
-    for pE in evt["pulseEnergies"]: plotting.line.plotHistory(pE)
+    analysis.beamline.averagePulseEnergy(evt, "pulseEnergies")
+    for pE in evt["pulseEnergies"].keys(): 
+        plotting.line.plotHistory(evt["pulseEnergies"][pE])
 
     # Get central 4 ASICS of CSPAD Ds2 detector in stack
     # --------------------------------------------------
-    evt["central4Asicis"] = analysis.pixel_detector.getCentral4Asics(cspad)
+    analysis.pixel_detector.getCentral4Asics(evt, "calibrated", "CsPad Ds2[calibrated]")
 
     # Plot Histogram of detector
     # --------------------------
-    plotting.line.plotHistogram(evt["central4Asics"], **histogram)
+    plotting.line.plotHistogram(evt["analysis"]["central4Asics"], **histogram)
 
 
     # Get total nr. of photons on the detector
     # ----------------------------------------
-    evt["nrPhotons"] = analysis.pixel_detector.totalNrPhotons(evt["central4Asics"], aduPhoton=state["aduPhoton"], aduThreshold=state["aduThreshold"])
-    plotting.line.plotHistory(evt["nrPhotons"])
+    analysis.pixel_detector.totalNrPhotons(evt, "analysis" , "central4Asics", aduPhoton, aduThreshold)
+    plotting.line.plotHistory(evt["analysis"]["nrPhotons - central4Asics"])
     
     # Plotting mean intenisty map for Aperture 1
     # ------------------------------------------
-    x,y = evt['parameters']['ap1_x'].data, evt['parameters']['ap1_y'].data 
-    print "Aperture 1, position in x: ", x
-    print "Aperture 1, position in y: ", y
-    msg_aperture1 = "Pos: (x,y) = (%.2f, %.2f)" %(x,y)
-    plotting.maps.plotMeanMap('aperture1', evt['parameters']['ap1_x'], evt['parameters']['ap1_y'], evt["nrPhotons"], evt["pulseEnergies"]["average"], msg=msg_aperture1, update=100, **meanIntensityAperture1)
-    #analysis.background.plotAperturePos(evt['parameters']['ap1_x'])
-    #analysis.background.plotAperturePos(evt['parameters']['ap1_y'])
-
-    # Plotting mean intenisty map for Aperture 2
-    # ------------------------------------------
-    x,y = evt['parameters']['ap2_x'].data, evt['parameters']['ap2_y'].data 
-    print "Aperture 2, position in x: ", x
-    print "Aperture 2, position in y: ", y
-    msg_aperture2 = "Pos: (x,y) = (%.2f, %.2f)" %(x,y)
-    plotting.maps.plotMeanMap('aperture2', evt['parameters']['ap2_x'], evt['parameters']['ap2_y'], evt["nrPhotons"], evt["pulseEnergies"]["average"], msg=msg_aperture3, update=100, **meanIntensityAperture3)
-    #analysis.background.plotAperturePos(evt['parameters']['ap2_x'])
-    #analysis.background.plotAperturePos(evt['parameters']['ap2_y'])
-
-    # Plotting mean intenisty map for Aperture 3
-    # ------------------------------------------
-    x,y = evt['parameters']['ap3_x'].data, evt['parameters']['ap2_y'].data 
-    print "Aperture 3, position in x: ", x
-    print "Aperture 3, position in y: ", y
-    msg_aperture3 = "Pos: (x,y) = (%.2f, %.2f)" %(x,y)
-    plotting.maps.plotMeanMap('aperture3', evt['parameters']['ap3_x'], evt['parameters']['ap3_y'], evt["totalIntensity"], evt["pulseEnergy"], msg=msg_aperture3, update=100, **meanIntensityAperture3)
-    #analysis.background.plotAperturePos(evt['parameters']['ap3_x'])
-    #analysis.background.plotAperturePos(evt['parameters']['ap3_y'])
+    x,y = evt['parameters']['ap1_x'], evt['parameters']['ap1_y']
+    print "Aperture 1, position in x: ", x.data
+    print "Aperture 1, position in y: ", y.data
+    print "Nr photons: ",  evt["analysis"]["nrPhotons - central4Asics"].data
+    print "Average Pulse Energy: ", evt["analysis"]["averagePulseEnergy"].data
+    msg_aperture1 = "Pos: (x,y) = (%.2f, %.2f)" %(x.data,y.data)
+    plotting.correlation.plotMeanMap(x, y, evt["analysis"]["nrPhotons - central4Asics"], evt["analysis"]["averagePulseEnergy"], msg=msg_aperture1, update=10, **meanIntensityAperture1)
+    plotting.line.plotHistory(evt['parameters']['ap1_x'])
+    plotting.line.plotHistory(evt['parameters']['ap1_y'])
         
     # Plotting mean intensity map for fixed target sample
     # ---------------------------------------------------
-    x,y = evt['parameters']['CXI:SC1:MMS:02.RBV'].data, evt['parameters']['CXI:USR:MMS:17.RBV'].data
-    print "Fixed target  position in x: ", x
-    print "Fixed target  position in y: ", y
-    msg_sample = "Pos: (x,y) = (%.2f, %.2f)" %(x,y)
-    plotting.maps.plotMeanMap('sample', evt['parameters']['CXI:SC1:MMS:02.RBV'], evt['parameters']['CXI:USR:MMS:17.RBV'], evt["nrPhotons"], evt["pulseEnergies"]["average"], msg=msg_sample, update=100, **meanIntensitySample)
-    #analysis.background.plotAperturePos(evt['parameters']['CXI:SC1:MMS:02.RBV'])
-    #analysis.background.plotAperturePos(evt['parameters']['CXI:USR:MMS:17.RBV'])
+    #x = evt['parameters']['CXI:SC1:MMS:02.RBV']
+    #y = evt['parameters']['CXI:USR:MMS:17.RBV']
+    #print "Fixed target  position in x: ", x.data
+    #print "Fixed target  position in y: ", y.data
+    #msg_sample = "Pos: (x,y) = (%.2f, %.2f)" %(x.data,y.data)
+    #plotting.correlation.plotMeanMap(x,y, evt["analysis"]["nrPhotons - central4Asics"], evt["analysis"]["averagePulseEnergy"], msg=msg_sample, update=100, **meanIntensitySample)
+    #plotting.line.plotHistory(evt['parameters']['CXI:SC1:MMS:02.RBV'])
+    #plotting.line.plotHistory(evt['parameters']['CXI:USR:MMS:17.RBV'])
     
     # Hitfinding
     # ----------
-    hit, evt["hitscore"] = analysis.hitfinding.countLitPixels(evt['calibrated']['CsPad Dg3 [calibrated]'], aduThreshold, litPixelThreshold)
-    plotting.line.plotHistory(evt["hitscore"])
-    if hit: plotting.image.plotImage(cspad)
+    analysis.hitfinding.countLitPixels(evt, 'calibrated', 'CsPad Ds2[calibrated]', aduThreshold, litPixelThreshold)
+    plotting.line.plotHistory(evt["analysis"]["hitscore - CsPad Ds2[calibrated]"])
+    if evt["analysis"]["isHit - CsPad Ds2[calibrated]"]: 
+        plotting.image.plotImage(cspad)
 
     # How fast are we processing the data?
     # ------------------------------------
