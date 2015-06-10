a71991e553e76da681764febff1a957c80a5f321
cxiopr <jana@slac.stanford.edu>
2015-06-10 12:50:37 -0700

    Revert "Change trap command to not do weird waiting"
    This reverts commit e8591c714cb2ec032860df42529ff67ea7e41028.

--- cxi86715/run.sh
+++ cxi86715/run.sh
@@ -3,7 +3,7 @@
 source .bashrc
 #/hummingbird.py -b examples/psana/mpi/conf.py
 
-trap 'kill -USR1 $PID' USR1
+trap 'kill -USR1 $PID; wait $PID' USR1
 ./hummingbird.py -b cxi86715/conf.py &
 PID=$!
 wait $PID
