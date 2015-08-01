10787caeed424cec42fd727bbec6281f4b1dc699
amoopr <jana@slac.stanford.edu>
2015-07-31 17:16:15 -0700

    Current state, after imprint taking.

--- archive/amo86615/conf0.py
+++ archive/amo86615/conf0.py
@@ -2,11 +2,15 @@ import time
 import analysis.event
 import analysis.stack
 import analysis.pixel_detector
+import analysis.hitfinding
 import plotting.image
+import plotting.line
 import utils.reader
 import os,sys
 this_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(this_dir)
+
+from backend import add_record
 
 # Collect and write out stacks of frames
 do_stacks = False
@@ -31,8 +35,8 @@ back_key  = "pnccdBack[%s]" % back_type
 # Backgrounds
 # -----------
 Nbg   = 100
-rbg   = 100
-obg   = 100
+rbg   = 10
+obg   = 10
 bg_front = analysis.stack.Stack(name="bg_front",maxLen=Nbg,outPeriod=obg,reducePeriod=rbg)
 bg_back = analysis.stack.Stack(name="bg_back",maxLen=Nbg,outPeriod=obg,reducePeriod=rbg)
 bg_dir = this_dir + "/stack"
@@ -60,6 +64,7 @@ def onEvent(evt):
 
     # Time measurement
     analysis.event.printProcessingRate()
+
     #analysis.event.printID(evt["eventID"])
     #print evt["photonPixelDetectors"].keys()
     #from IPython.core.debugger import Tracer
@@ -81,6 +86,32 @@ def onEvent(evt):
         front_type_s = front_type
         front_key_s = front_key
 
+    # Simple hitfinding (Count Nr. of lit pixels)
+    analysis.hitfinding.countLitPixels(evt, back_type, back_key, aduThreshold=20, hitscoreThreshold=50, mask=mask_back)
+
+    # Compute the hitrate
+    analysis.hitfinding.hitrate(evt, evt["analysis"]["isHit - " + back_key], history=10000)
+    
+    # Plot the hitscore
+    #plotting.line.plotHistory(evt["analysis"]["hitscore - " + back_key], label='Nr. of lit pixels')
+    plotting.line.plotHistory(evt["analysis"]["hitscore - " + back_key], runningHistogram=True, hmin=0, hmax=100000, bins=100, window=100, history=1000)
+
+    # Plot the hitrate
+    plotting.line.plotHistory(evt["analysis"]["hitrate"], label='Hit rate [%]')
+
+
+    hit = evt["analysis"]["isHit - " + back_key]
+
+    # Pulse Energy
+    #plotting.line.plotHistory(evt["analysis"]["averagePulseEnergy"])
+
+    # Perform sizing on hits
+    if evt["analysis"]["isHit - " + back_key].data:
+        plotting.image.plotImage(evt[front_type_s][front_key_s], 
+                                 msg='', name="pnCCD front (hit)", vmin=0, vmax=10000, mask=mask_front)     
+        plotting.image.plotImage(evt[back_type_s][back_key_s], 
+                                 msg='', name="pnCCD back (hit)", vmin=0, vmax=10000, mask=mask_back) 
+
     plotting.image.plotImage(evt[front_type_s][front_key_s], 
                              msg='', name="pnCCD front", vmin=0, vmax=10000, mask=mask_front)     
     plotting.image.plotImage(evt[back_type_s][back_key_s], 
@@ -91,11 +122,34 @@ def onEvent(evt):
     # COLLECTING BACKGROUND
     if do_stacks:
         # Update
-        bg_front.add(evt[front_type][front_key].data)
-        bg_back.add(evt[back_type][back_key].data)
+        bg_front.add(evt[front_type_s][front_key_s].data)
+        bg_back.add(evt[back_type_s][back_key_s].data)
         # Reduce
         bg_front.reduce()
         bg_back.reduce()
         # Write to file
-        bg_front.write(evt,directory=bg_dir)
-        bg_back.write(evt,directory=bg_dir)
+        #bg_front.write(evt,directory=bg_dir)
+        #bg_back.write(evt,directory=bg_dir)
+
+
+        #print data
+        data = bg_front.mean()
+        bg_front_mean = add_record(evt["analysis"], "analysis", "pnCCD front (mean)", data, unit='')
+        plotting.image.plotImage(evt["analysis"]["pnCCD front (mean)"], 
+                                 msg='', name="pnCCD front (mean)", vmin=0, vmax=10000)     
+        data = bg_front.max()
+        bg_front_max = add_record(evt["analysis"], "analysis", "pnCCD front (max)", data, unit='')
+        plotting.image.plotImage(evt["analysis"]["pnCCD front (max)"], 
+                                 msg='', name="pnCCD front (max)", vmin=0, vmax=10000)     
+
+        data = bg_back.mean()
+        bg_back_mean = add_record(evt["analysis"], "analysis", "pnCCD back (mean)", data, unit='')
+        plotting.image.plotImage(evt["analysis"]["pnCCD back (mean)"], 
+                                 msg='', name="pnCCD back (mean)", vmin=0, vmax=10000)     
+        data = bg_back.max()
+        bg_back_max = add_record(evt["analysis"], "analysis", "pnCCD back (max)", data, unit='')
+        plotting.image.plotImage(evt["analysis"]["pnCCD back (max)"], 
+                                 msg='', name="pnCCD back (max)", vmin=0, vmax=10000)     
+
+
+    
