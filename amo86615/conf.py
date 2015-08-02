49336706b430f6469c4f08c512a1bdc6ea1a0c7f
amoopr <jana@slac.stanford.edu>
2015-08-02 07:39:02 -0700

    Start of third shift (amoopr)

--- amo86615/conf.py
+++ amo86615/conf.py
@@ -5,6 +5,7 @@ import analysis.pixel_detector
 import analysis.hitfinding
 import plotting.image
 import plotting.line
+import plotting.correlation
 import utils.reader
 import os,sys
 this_dir = os.path.dirname(os.path.realpath(__file__))
@@ -17,6 +18,9 @@ from backend import add_record
 do_stacks = False
 # Common mode correction along fastest changing dimension
 do_cmc = True
+# Send all events to the frontend
+do_showall  = True
+show_prop = 0.01
 
 # ---------------------------------------------------------
 # P S A N A
@@ -26,6 +30,15 @@ state['Facility'] = 'LCLS'
 #state['LCLS/DataSource'] = 'exp=amo86615:run=3'
 state['LCLS/DataSource'] = 'shmem=psana.0:stop=no'
 state['LCLS/PsanaConf'] = this_dir + '/psana_cfg/pnccd.cfg'
+
+# INJECTOR MOTORS
+# ---------------
+injector_x_key = "AMO:PPL:MMS:07.RBV"
+injector_z_key = "AMO:PPL:MMS:08.RBV"
+
+# ---------------------------------------------------------
+# I/O
+# ---------------------------------------------------------
 
 front_type = "image"
 front_key  = "pnccdFront[%s]" % front_type
@@ -54,6 +67,33 @@ mask_back = M_back.boolean_mask
 M_front    = utils.reader.MaskReader(this_dir + "/mask/mask_front.h5","/data/data")
 mask_front = M_front.boolean_mask
 (ny_front,nx_front) = mask_front.shape
+
+
+
+# ---------------------------------------------------------
+# PLOTTING PARAMETER
+# ---------------------------------------------------------
+
+# Injector position limits
+x_min = 4.
+x_max = 8.
+x_bins = 50
+z_min = -2.
+z_max = 2
+z_bins = 100
+
+# Hitrate mean map 
+hitrateMeanMapParams = {
+    'xmin': x_min,
+    'xmax': x_max,
+    'ymin': z_min,
+    'ymax': z_max,
+    'xbins': x_bins,
+    'ybins': z_bins,
+    'xlabel': 'Injector Position in x',
+    'ylabel': 'Injector Position in z'  
+}
+
 
 # ---------------------------------------------------------
 # E V E N T   C A L L
@@ -94,7 +134,7 @@ def onEvent(evt):
         front_key_s = front_key
 
     # Simple hitfinding (Count Nr. of lit pixels)
-    analysis.hitfinding.countLitPixels(evt, back_type, back_key, aduThreshold=100, hitscoreThreshold=1500, hitscoreMax=4500, mask=mask_back)
+    analysis.hitfinding.countLitPixels(evt, back_type, back_key, aduThreshold=100, hitscoreThreshold=3500, hitscoreMax=4500, mask=mask_back)
     #analysis.hitfinding.countLitPixels(evt, back_type, back_key, aduThreshold=1600, hitscoreThreshold=1500, hitscoreMax=4500, mask=mask_back)
 
     # Compute the hitrate
@@ -125,8 +165,10 @@ def onEvent(evt):
                                  msg='', name="pnCCD front", vmin=0, vmax=10000, mask=mask_front)     
         plotting.image.plotImage(evt[back_type_s][back_key_s], 
                                  msg='', name="pnCCD back", vmin=0, vmax=10000, mask=mask_back)     
-    #print evt[front_type][front_key].data.shape
-    #print evt[back_type][back_key].data.shape
+
+    # Plot MeanMap of hitrate(y,z)
+    #plotting.correlation.plotMeanMap(evt["parameters"][injector_x_key], evt["parameters"][injector_z_key], hit, plotid='hitrateMeanMap', **hitrateMeanMapParams)
+    plotting.correlation.plotMeanMap(evt["parameters"][injector_x_key], evt["parameters"][injector_z_key], 1, plotid='hitrateMeanMap', **hitrateMeanMapParams)
 
     # COLLECTING BACKGROUND
     if do_stacks:
