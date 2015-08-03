fdd021a0dfe03bc02f3ddd41e70eddd5d0e78631
amoopr <jana@slac.stanford.edu>
2015-08-03 09:44:48 -0700

    New thresholds for hitfinding with different gains

--- amo86615/run.sh
+++ amo86615/run.sh
@@ -4,6 +4,10 @@ cd /reg/neh/operator/amoopr/amo86615/hummingbird/
 
 source .bashrc
 
+#ulimit -a
+#ulimit -m 5000000
+#ulimit -H -v 5000000
+
 trap 'kill -USR1 $PID' USR1
 
 ./hummingbird.py -b /reg/neh/operator/amoopr/amo86615/hummingbird/amo86615/conf.py &
